af44e0cfa989ba89dd94d172e33ed9e57d193904
Sagar Choudhary <sagar.choudhary96@gmail.com>
2025-02-23 22:24:46 +0530

    Add tooltip component

---
+++ components/Tooltip/index.tsx
@@ -0,0 +1,25 @@
+import {
+  Tooltip as TooltipWrapper,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+type TooltipProps = {
+  label: string;
+  children: React.ReactNode;
+};
+const Tooltip = ({ label, children }: TooltipProps) => {
+  return (
+    <TooltipProvider>
+      <TooltipWrapper>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent>
+          <p>{label}</p>
+        </TooltipContent>
+      </TooltipWrapper>
+    </TooltipProvider>
+  );
+};
+
+export default Tooltip;
